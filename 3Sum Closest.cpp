a7ab75c649ee728a4cf35c901c587ad737d5fc57
char <char@aladinfun.com>
2018-10-10 21:03:38 +0800

    add more soluition

--- 3Sum Closest.cpp
+++ 3Sum Closest.cpp
@@ -1,12 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> threeSumClosest(vector<int>& nums, int target) {
+    vector<int> threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int sum = 0;
-        for (int i = 0, j = nums.size() - 1; i < j; ) {
-            if (nums[j] + nums[i] > target) {
-                j--;
+        int diff = INT32_MAX;
+        int left, right, mid;
+        int size = nums.size();
+        vector<int> arr;
+
+        for (int i = 0; i < size - 1; i++) {
+            if (nums[i] >= 0 && nums[i] + nums[size-1] > target) {
+                left = i;
+                mid = left + 1;
+                right = mid + 1;
+                arr = {nums[left], nums[right], nums[mid]};
+                return arr;
             }
+
+            int l = i + 1;
+            int r = size - 1;
+
+            while(r > l) {
+                int tmp = nums[i] + nums[r] + nums[l] - target;
+                if (tmp == 0) {
+                    left = i;
+                    right = r;
+                    mid = l;
+                    arr = {nums[left], nums[right], nums[mid]};
+                    return arr;
+                }
+
+                diff = (diff < tmp)?diff:tmp;
+
+                if (tmp > 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+
         }
+
+        return
     }
 };
--- 3Sum.cpp
+++ 3Sum.cpp
@@ -3,34 +3,38 @@
 // 因为数组已经排序，所以每次遍历开始的时候，只需要检测当前元素右边的组合是否满足条件即可
 // 找出满足条件的组合的关键：双指针分别指向数组末尾以及下一个元素，如果两个元素之和大于当前元素，说明右侧太大应该减少
 // 每次完成一个元素的查找，需要跳过下一个重复的元素
+// 注意空数组的情况
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int i = 0, j, k;
-        vector<vector<int> > result;
-        if (nums.size() < 3)
-        	return result;
-        sort(nums.begin(), nums.end()); 
-        while(i < nums.size() - 2 && nums[i] <= 0)
-        {
-            j = i + 1;
-            k = nums.size() - 1;
-            while (j < k)
-            {
-                if (nums[j] + nums[k] == -nums[i])
-                {
-                    vector<int> vecTemp = {nums[i], nums[j], nums[k]};
-                    result.push_back(vecTemp);
-                    while(nums[j] == nums[--j]){}
-                    while(nums[k] == nums[--k]){}
-                }
-                else if (nums[j] + nums[k] > -nums[i])
-                    k--;
-                else
-                    j++;
-            }
-            while(nums[i] == nums[++i]) {}
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> arrs;
+        if (nums.size() < 3) {
+            return arrs;
         }
-        return result;
+
+        int left, right;
+        for (int i = 0; i < nums.size() - 1 && nums[i] <= 0;) {
+            int target = -nums[i];
+            int left = i+1;
+            int right = nums.size() - 1;
+            while (right > left) {
+                if (nums[left] + nums[right] == target) { // 相加为零
+                    vector<int> arr = {nums[left], nums[right], nums[i]};
+                    arrs.push_back(arr);
+                    while(nums[right] == nums[--right]) {};
+                    while(nums[left] == nums[++left]) {};
+                } else if (nums[left] + nums[right] > target) { // 右边太大
+                    right--;
+                } else { // 左边太大
+                    left++;
+                }
+
+
+            }
+            while(nums[i]==nums[++i]){};
+        }
+        return arrs;
     }
 };
